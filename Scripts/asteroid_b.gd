fa4dae256f59071642a33514129a79c02888db82
Rodrigo_Scipioni <rodrigoscipioni1@gmail.com>
2025-11-12 11:48:32 -0300

    scripts de asteroides depurado

--- Scripts/asteroid_b.gd
+++ Scripts/asteroid_b.gd
@@ -1,21 +1,24 @@
 extends Node2D
 @export var planeta: Node2D
-@export var velocidad_orbita = 2   # Qué tan rápido orbita (radianes/seg)
-@export var velocidad_rotacion = 2 # Qué tan rápido gira sobre sí mismo
-@export var radio_a = 200          # El radio horizontal de la elipse
-@export var radio_b = 200          # El radio vertical de la elipse
+@export var velocidad_orbita = 2
+@export var velocidad_rotacion = 2
+@export var radio_h = 200
+@export var radio_v = 200
 var progreso_orbita = 0
 @onready var sprite = $Sprite2D
 
 func _process(delta):
 	progreso_orbita += velocidad_orbita * delta
-	# 2. Calcular la nueva posición usando trigonometría (fórmula de la elipse)
+
 	if planeta:
-		var centro = planeta.global_position
-		var nueva_posicion = Vector2()
-		nueva_posicion.x = centro.x + radio_a * cos(progreso_orbita)
-		nueva_posicion.y = centro.y + radio_b * sin(progreso_orbita)
-		global_position = nueva_posicion
-	# Girar el sprite sobre su propio eje
+		orbitar()
+
 	if sprite:
 		sprite.rotate(velocidad_rotacion * delta)
+
+func orbitar():
+	var centro = planeta.global_position
+	var nueva_posicion = Vector2()
+	nueva_posicion.x = centro.x + radio_h * cos(progreso_orbita)
+	nueva_posicion.y = centro.y + radio_v * sin(progreso_orbita)
+	global_position = nueva_posicion
