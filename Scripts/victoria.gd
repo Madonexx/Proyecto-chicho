9efb4f67b982ff74366eced622df1618964f1728
Rodrigo_Scipioni <rodrigoscipioni1@gmail.com>
2025-11-12 12:51:59 -0300

    Script victoria depurado

--- Scripts/victoria.gd
+++ Scripts/victoria.gd
@@ -1,17 +1,7 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
 func _on_button_pressed() -> void:
 	SceneManager.cambiar_escena("res://Escenas/main.tscn")
-	
 
 func _on_button_2_pressed() -> void:
 	SceneManager.cambiar_escena("res://Escenas/MainMenu.tscn")
-	
